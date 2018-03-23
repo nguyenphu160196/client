8370b471419766d61f72c0d98977a4dc0e24d822
ThienPhu <nguyenphu160196@gmail.com>
2018-03-23 20:37:01 +0700

    login with facebook

--- src/routes/Login/components/Login.js
+++ src/routes/Login/components/Login.js
@@ -8,8 +8,11 @@ export const Login = ({ login, handleLogin, makeState, handleSignup}) => (
 					event.preventDefault();
 					handleLogin()
 				}}>
-      <button className="btn btn-success" type="submit">Login</button>      
+      <button className="btn btn-success" type="submit">Login</button> 
+      <div className="fb-login-button" data-max-rows="1" data-size="medium" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="true"></div>     
     </form>
+    
+    
     {/* <form onSubmit={(event)=>{
 					event.preventDefault();
 					handleSignup()
