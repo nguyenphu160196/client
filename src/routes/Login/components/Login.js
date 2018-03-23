a4990e3b8524298abca3c114344ba75d0f1a54e6
ThienPhu <nguyenphu160196@gmail.com>
2018-03-23 21:01:37 +0700

    login face

--- src/routes/Login/components/Login.js
+++ src/routes/Login/components/Login.js
@@ -9,15 +9,8 @@ export const Login = ({ login, handleLogin, makeState, handleSignup}) => (
 					handleLogin()
 				}}>
       <button className="btn btn-success" type="submit">Login</button>  
+      <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="true"></div>
     </form>
-
-    <div
-          className="fb-login-button"
-          data-max-rows="1"
-          data-size="large"
-          data-button-type="continue_with"
-          data-use-continue-as="true"
-          ></div>
     
     
     {/* <form onSubmit={(event)=>{
