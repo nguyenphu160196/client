7dc7581a4be8275b7f01d0c13e2d7a7512a3791b
ThienPhu <nguyenphu160196@gmail.com>
2018-04-06 19:57:40 +0700

    update api

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'https://server-cut-air.herokuapp.com/api/',
+  // baseURL: 'http://localhost:9090/api/',
   timeout: 60000
 });
 
